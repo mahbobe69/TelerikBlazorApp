689bf01d9dbbbc4b93ca690e75aab449431c7c9d
Mahbobe Armanfard <mhbbarman@outlook.com>
2021-05-22 23:33:06 +0430

    AddProject

---
+++ TelerikBlazorApp/Server/Controllers/ProductDataController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TelerikBlazorApp.Shared;
+
+namespace TelerikBlazorApp.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductDataController : ControllerBase
+    {
+        private List<Product> products = ProductViewModel.Getproducts();
+        private List<ProductViewModel> buys = new List<ProductViewModel>();
+        public ProductDataController()
+        {
+            Random random = new Random();
+            if (buys.Count == 0)
+                Enumerable.Range(1, 50).ToList().ForEach(x =>
+                {
+                    int num = random.Next(0, products.Count);
+                    buys.Add(new ProductViewModel()
+                    {
+                        Name = products[num].Name,
+                        Category = products[num].Category,
+                        Price = products[num].Price,
+                        Date = DateTime.Now.AddDays(-x),
+                        Number = random.Next(1, 1500),
+                        BuyCount = num * random.Next(1, 50)
+                    });
+                });
+        }
+        [HttpGet]
+        [Route("AllBuys")]
+        public async Task<List<ProductViewModel>> AllBuys()
+        {
+            return await Task.FromResult(buys);
+        }
+        [HttpGet]
+        [Route("Products")]
+        public async Task<List<Product>> AllProducts()
+        {
+            return await Task.FromResult(products);
+        }
+        [HttpGet]
+        [Route("BuyMaterial")]
+        public async Task<int> BuyMaterial()
+        {
+            return await Task.FromResult(buys.Sum(x => x.BuyCount));
+        }
+        [HttpGet]
+        [Route("GetEarning")]
+        public async Task<decimal> GetEarning([FromQuery] bool ByMonth)
+        {
+            var date = DateTime.Now;
+            return await Task.FromResult(buys.Where(x => ByMonth ? x.Date.Month == date.Month : x.Date.Year == date.Year).Sum(x => x.Price));
+        }
+        [HttpGet]
+        [Route("ProductData")]
+        public async Task<IEnumerable<ProductViewModel>> ProductData([FromQuery] bool byCategory)
+        {
+            var CategoryViews = buys.GroupBy(x => byCategory == true ? x.Category : x.Name)
+                                .Select(x => new ProductViewModel
+                                {
+                                    Category = x.Key,
+                                    BuyCount = x.Sum(v => v.BuyCount)
+                                });
+
+            return await Task.FromResult(CategoryViews);
+        }
+
+    }
+}
---
+++ TelerikBlazorApp/Shared/ProductViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelerikBlazorApp.Shared
+{
+    public class ProductViewModel
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public decimal Price { get; set; }
+        public int BuyCount { get; set; }
+        public DateTime Date { get; set; }
+        public string Category { get; set; }
+        public static List<Product> Getproducts()
+        {
+        return new List<Product>() {
+        new Product(name:"گوشی سامسونگ A 31",price:6000000,category:"سامسونگ"),
+        new Product(name: "گوشی سامسونگ A 51", price: 8000000, category: "سامسونگ"),
+        new Product(name: "گوشی سامسونگ A 71", price: 10000000, category: "سامسونگ"),
+        new Product(name: "گوشی سامسونگ S 21", price: 6000000, category: "سامسونگ"),
+        new Product(name: "گوشی اپل 12", price: 360000000, category: "آیفون"),
+        new Product(name: "گوشی اپل 11", price: 2400000, category: "آیفون"),
+        new Product(name: "شیاوومی note 9", price: 600000, category: "شیاوومی"),
+        new Product(name: "شیاوومی redmi", price: 400000, category: "شیاوومی")
+        };
+        }
+    }
+    public class Product
+    {
+        public Product(string name, decimal price, string category)
+        {
+            Name = name;
+            Price = price;
+            Category = category;
+        }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+
+}
