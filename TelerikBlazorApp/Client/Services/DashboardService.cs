1076860827521f925adc148572e0df28104d2275
Mahbobe Armanfard <mhbbarman@outlook.com>
2021-05-22 23:34:06 +0430

    AddProject

---
+++ TelerikBlazorApp/Client/Services/DashboardService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using TelerikBlazorApp.Shared;
+
+namespace TelerikBlazorApp.Client.Services
+{
+    public class DashboardService : IDashboardService
+    {
+        private HttpClient _httpclient { set; get; }
+        public DashboardService(HttpClient httpClient)
+        {
+            _httpclient = httpClient;
+        }
+        public Task<List<ProductViewModel>> AllBuys()
+        {
+            return  _httpclient.GetFromJsonAsync<List<ProductViewModel>>("ProductData/AllBuys");
+        }
+
+        public Task<List<Product>> AllProducts()
+        {
+            return _httpclient.GetFromJsonAsync<List<Product>>("ProductData/AllProducts");
+
+        }
+
+        public Task<int> BuyMaterial()
+        {
+            return _httpclient.GetFromJsonAsync<int>("ProductData/BuyMaterial");
+
+        }
+
+        public Task<decimal> GetEarning(bool ByMonth)
+        {
+            return _httpclient.GetFromJsonAsync<decimal>("ProductData/GetEarning?ByMonth="+ByMonth);
+
+        }
+
+        public async Task<IEnumerable<ProductViewModel>> ProductData(bool byCategory)
+        {
+            return await _httpclient.GetFromJsonAsync<IEnumerable<ProductViewModel>>("ProductData/ProductData?byCategory=" + byCategory);
+        }
+    }
+}
---
+++ TelerikBlazorApp/Client/Services/IDashboardService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TelerikBlazorApp.Shared;
+
+namespace TelerikBlazorApp.Client.Services
+{
+    interface IDashboardService
+    {
+        Task<List<ProductViewModel>> AllBuys();
+        Task<List<Product>> AllProducts();
+        Task<int> BuyMaterial();
+        Task<decimal> GetEarning(bool ByMonth);
+        Task<IEnumerable<ProductViewModel>> ProductData(bool ByCategory);
+    }
+}
